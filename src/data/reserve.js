add3643d05a19b02c030c0ae8deacf5d99ec4608
Lawliet <chawkanijairi8@gmail.com>
2025-11-06 23:03:45 +0800

    update scr/data/reserve.js

--- src/data/reserve.js
+++ src/data/reserve.js
@@ -1,9 +1,13 @@
-import smartphoneImg from "../assets/smartphone.jpeg";
-import laptopImg from "../assets/laptop.jpg";
-import headphonesImg from "../assets/headphones.jpeg";
-import computerImg from "../assets/computer.jpeg";
-import IpadImg from "../assets/Ipad.jpeg";
-import TVImg from "../assets/TV.jpeg";
+import samsungImg from "../assets/samsung.jpeg";
+import asusImg from "../assets/asus.jpg";
+import sonyImg from "../assets/sony.jpg";
+import dellImg from "../assets/dell.jpeg";
+import miniImg from "../assets/mini.png";
+import oledImg from "../assets/oled.jpg";
+import gooleImg from "../assets/google.jpg";
+import jblImg from "../assets/jbl.jpg";
+import pavillionImg from "../assets/pavillion.jpeg";
+import tlcImg from "../assets/tlc.png"
 
 export const reserveProducts = [
   {
@@ -11,69 +15,69 @@ export const reserveProducts = [
     name: "Samsung Galaxy S24",
     price: 4200,
     description: "A modern smartphone with amazing display and performance.",
-    image: smartphoneImg,
+    image: samsungImg,
   },
   {
     id: 8,
     name: "ASUS Zenbook 14",
     price: 7600,
     description: "Lightweight laptop designed for work and entertainment.",
-    image: laptopImg,
+    image: asusImg,
   },
   {
     id: 9,
     name: "Sony WH-1000XM5",
     price: 380,
     description: "Top-tier noise-cancelling headphones with clear sound.",
-    image: headphonesImg,
+    image: sonyImg,
   },
   {
     id: 10,
     name: "Dell Inspiron 27",
     price: 8500,
     description: "All-in-one desktop PC built for performance and simplicity.",
-    image: computerImg,
+    image: dellImg,
   },
   {
     id: 11,
     name: "iPad Mini 6",
     price: 2900,
     description: "Compact yet powerful tablet for work and creativity.",
-    image: IpadImg,
+    image: miniImg,
   },
   {
     id: 12,
     name: "LG OLED Smart TV",
     price: 10800,
     description: "Cinematic 4K display with built-in streaming apps.",
-    image: TVImg,
+    image: oledImg,
   },
   {
     id: 13,
     name: "Google Pixel 8",
     price: 3900,
     description: "Smart and AI-enhanced camera experience in your pocket.",
-    image: smartphoneImg,
+    image: gooleImg,
   },
   {
     id: 14,
     name: "HP Pavilion 15",
     price: 6800,
     description: "Powerful laptop for study, work, and entertainment.",
-    image: laptopImg,
+    image: pavillionImg,
   },
   {
     id: 15,
     name: "JBL Live 660NC",
     price: 250,
     description: "Stylish wireless headphones with deep bass sound.",
-    image: headphonesImg,
+    image: jblImg,
   },
   {
     id: 16,
     name: "TCL QLED 55-inch",
     price: 9400,
     description: "Vibrant QLED Smart TV built for an immersive experience.",
-    image: TVImg,
+    image: tlcImg,
   },
 ];
