5daf597d8786787fb771c548e95b7f21439e0ddc
Lawliet <chawkanijairi8@gmail.com>
2025-11-08 00:05:58 +0800

    update frontend

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+// const apiClient = axios.create({
+//   baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
+// });
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://ecommerce-backend-lxq0.onrender.com/api",
 });
 
 export default apiClient;
